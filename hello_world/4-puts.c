124be1058257270a4cdb0326ea7f86241d047187
danmece12 <danielmece@unyt.edu.al>
2025-06-09 11:03:05 +0000

    Task 4: fix comment blank lines for Betty-doc

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * prints the puzzle quote using puts()
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
