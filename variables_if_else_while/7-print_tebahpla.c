55dca5815840cdfcf2b53890472fa390ca0dade0
danmece12 <danielmece@unyt.edu.al>
2025-06-11 10:24:06 +0000

    Fix Betty style: remove leading spaces and use tabs in 7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints the lowercase alphabet in reverse, followed by a newline
- *
- * Return: 0 on success
- */
+* main - prints the lowercase alphabet in reverse, followed by a newline
+*
+* Return: 0 on success
+*/
 int main(void)
 {
 	char c;
@@ -12,5 +12,5 @@ int main(void)
 	for (c = 'z'; c >= 'a'; c--)
 		putchar(c);
 	putchar('\n');
-    return (0);
+	return (0);
 }
