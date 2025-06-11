f7105ba28d1b9ca55b0fad1183e533a193440001
danmece12 <danielmece@unyt.edu.al>
2025-06-11 09:23:28 +0000

    Fix indentation to tabs for Betty style in 6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
-* main - prints all single digit numbers of base 10 starting from 0
-*
-* Return: 0 on success
-*/
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
-	    putchar('0' + i);
+		putchar('0' + i);
 	putchar('\n');
 	return (0);
 }
