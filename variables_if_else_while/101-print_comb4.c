f4fed7107c9da3d7f1f7d4799504a2d084088d6a
Daniel Mece <danmece12@example.com>
2025-07-20 12:40:37 +0000

    Print all unique combinations of 3 digits (012 to 789) in ascending order , separated by ', ', followed by new line.

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
+
 /**
-* main - prints all unique combinations of 3 digits (012 to 789)
-*        in ascending order, separated by ", ", followed by a newline
-*
-* Return: Always 0.
-*/
+ * main - prints all unique combinations of three digits (012 to 789)
+ *        in ascending order, separated by ", ", followed by a newline
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    int d1, d2, d3;
+	int d1, d2, d3;
 
-    for (d1=0; d1<=7; d1++)
-    {
-        for (d2=d1+1; d2<=8; d2++)
-        {
-            for (d3=d2+1; d2<=9; d3++)
-            {
-                putchar('0' +d1); /* #1 */
-                putchar('0'+ d2); /* #2 */
-                putchar('0' +d3); /* #3 */
-                if (!(d1 ==7 && d2==8 && d3 ==9))
-                {
-                    putchar(','); /* #4 */
-                    putchar(' '); /* #5 */
-
-                }
-            }
-        }
-    }
-    putchar('\n'); /* #6 */
-
-    return (0);
+	for (d1 = 0; d1 <= 7; d1++)
+	{
+		for (d2 = d1 + 1; d2 <= 8; d2++)
+		{
+			for (d3 = d2 + 1; d3 <= 9; d3++)
+			{
+				putchar('0' + d1);
+				putchar('0' + d2);
+				putchar('0' + d3);
+				if (!(d1 == 7 && d2 == 8 && d3 == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
