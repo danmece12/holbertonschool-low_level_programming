a4c9c7f5c2d088f110bbdad8e4079f08f56e097a
danmece12 <danielmece@unyt.edu.al>
2025-08-04 17:45:02 +0000

    finally

--- function_pointers/0-main.c
+++ function_pointers/0-main.c
@@ -4,24 +4,24 @@
 
 void print_name_as_is(char *name)
 {
-    printf("Hello , my name is %s\n", name);
+printf("Hello , my name is %s\n", name);
 }
 
 void print_name_uppercase(char *name)
 {
-    char c;
-    printf("Hello, my uppercase name is");
-    while ((c = *name++))
-    {
-        putchar(toupper((unsigned char)c));
-    }
-    putchar('\n');
+char c;
+printf("Hello, my uppercase name is");
+while ((c = *name++))
+{
+putchar(toupper((unsigned char)c));
+}
+putchar('\n');
 }
 
 
 int main(void)
 {
-    print_name("Bob Dylan", print_name_as_is);
-    print_name("Bob Dylan", print_name_uppercase);
-    return (0);
+print_name("Bob Dylan", print_name_as_is);
+print_name("Bob Dylan", print_name_uppercase);
+return (0);
 }
