2ce7e1fdb290da1e52c6488feb2a14805aa1304c
danmece12 <danielmece@unyt.edu.al>
2025-08-04 19:05:19 +0000

    2

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,11 +2,12 @@
 #include "function_pointers.h"
 
 /**
-* array_iterator - applies a callbak to each element of an int array
+* @array_iterator - applies a callbak to each element of an int array
 * @array: pointer to the first element
 * @size: number of elements in array
-* @actions: function pointer to apply to each element
-*/
+* @action: function pointer to apply to each element
+**/
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i;
