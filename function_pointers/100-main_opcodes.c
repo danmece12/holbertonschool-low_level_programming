8e420ef044c8698416274200f93ba20662221927
danmece12 <danielmece@unyt.edu.al>
2025-08-06 11:18:27 +0000

    4

--- function_pointers/100-main_opcodes.c
+++ function_pointers/100-main_opcodes.c
@@ -14,33 +14,33 @@
  */
 int main(int argc, char **argv)
 {
-    int n, i;
-    unsigned char *p;
+int n, i;
+unsigned char *p;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 
-    n = atoi(argv[1]);
-    if (n < 0)
-    {
-        printf("Error\n");
-        return (2);
-    }
+n = atoi(argv[1]);
+if (n < 0)
+{
+printf("Error\n");
+return (2);
+}
 
-    /* take the address of main as a pointer to raw bytes */
-    p = (unsigned char *)((uintptr_t)main);
+/* take the address of main as a pointer to raw bytes */
+p = (unsigned char *)((uintptr_t)main);
 
-    /* print n bytes in two-digit lowercase hex separated by spaces */
-    for (i = 0; i < n; i++)
-    {
-        printf("%02x", p[i]);
-        if (i + 1 < n)
-            putchar(' ');
-    }
-    putchar('\n');
+/* print n bytes in two-digit lowercase hex separated by spaces */
+for (i = 0; i < n; i++)
+{
+printf("%02x", p[i]);
+if (i + 1 < n)
+putchar(' ');
+}
+putchar('\n');
 
-    return (0);
+return (0);
 }
