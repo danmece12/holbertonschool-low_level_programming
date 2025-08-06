1a4473fc3c7a795f7a48665b4fd8b6d507ab3735
danmece12 <danielmece@unyt.edu.al>
2025-08-06 10:31:13 +0000

    Ex 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -7,16 +7,14 @@
  * Return: pointer to the matching function, or NULL if none match
  * Allowed: 1 if, no loops, no switch, no else, no goto
  */
- 
 int (*get_op_func(char *s))(int, int)
 {
-    if (s == NULL || s[0] == '\0' || s[1] != '\0')
-        return (NULL);
-
-    return (s[0] == '+' ? op_add  :
-            s[0] == '-' ? op_sub  :
-            s[0] == '*' ? op_mul  :
-            s[0] == '/' ? op_div  :
-            s[0] == '%' ? op_mod  :
-                          NULL);
+if (s == NULL || s[0] == '\0' || s[1] != '\0')
+return (NULL);
+return (s[0] == '+' ? op_add  :
+s[0] == '-' ? op_sub  :
+s[0] == '*' ? op_mul  :
+s[0] == '/' ? op_div  :
+s[0] == '%' ? op_mod  :
+NULL);
 }
